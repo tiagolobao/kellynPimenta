1433b470af4202e13704de5731931d5fb1d57830
Tiago Britto Lobão <tiago.blobao@gmail.com>
2019-10-08 14:36:39 -0300

    Últimos Ajustes <Deploy 1>

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,20 @@
-(function () {
+(function(){
+
+  //let fullBio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  document.getElementsByClassName('split-bio')[0].addEventListener('click',function(){
+    lists = document.getElementsByClassName('split-lists')[0];
+    lists.classList.toggle('hideOpacity');
+    setTimeout(function(){
+      lists.style.display = 'none';
+    },1000);
+
+    //textBio = document.querySelector('div.split-bio p');
+    //while( textBio.innerText.length > 0 ){
+    //  textBio.innerText = textBio.innerText.slice(0, -1);
+    //}
+
+
+  });
+
 })();
--- resources/precompile.sh
+++ resources/precompile.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo precompiling...
 sass style.scss:../assets/style.css
-tsc script.ts
-rm ../assets/script.js
-mv script.js ../assets/script.js
