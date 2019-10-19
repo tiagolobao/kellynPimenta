1ce87a6e47af283724df3a2e5f76e86a9ba0139f
Tiago Britto Lobão <tiago.blobao@gmail.com>
2019-10-19 10:22:08 -0300

    Colocando informações

--- assets/script.js
+++ assets/script.js
@@ -2,19 +2,33 @@
 
   //let fullBio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  document.getElementsByClassName('split-bio')[0].addEventListener('click',function(){
-    lists = document.getElementsByClassName('split-lists')[0];
-    lists.classList.toggle('hideOpacity');
-    setTimeout(function(){
-      lists.style.display = 'none';
-    },1000);
+  // document.getElementsByClassName('split-bio')[0].addEventListener('click',function(){
+  //   lists = document.getElementsByClassName('split-lists')[0];
+  //   lists.classList.toggle('hideOpacity');
+  //   setTimeout(function(){
+  //     lists.style.display = 'none';
+  //   },1000);
+  // });
 
-    //textBio = document.querySelector('div.split-bio p');
-    //while( textBio.innerText.length > 0 ){
-    //  textBio.innerText = textBio.innerText.slice(0, -1);
-    //}
-
-
+  document.querySelector('#gallery').addEventListener('click',function(){
+    location.href = 'https://www.instagram.com/kellynpimentaoliveira/';
   });
+
+  window.showText = function() {
+    document.querySelector('.split-lists').style['transition-duration'] = '0s';
+    document.querySelector('.split-lists').style.display = 'none';
+    document.querySelector('#leiaMais').style.display = 'none';
+    document.querySelectorAll('.hide-text')[0].style.display = 'block';
+    document.querySelectorAll('.hide-text')[1].style.display = 'block';
+    document.querySelectorAll('.hide-text')[2].style.display = 'block';
+  }
+
+  window.hideText = function() {
+    document.querySelectorAll('.hide-text')[1].style.display = 'none';
+    document.querySelectorAll('.hide-text')[2].style.display = 'none';
+    document.querySelectorAll('.hide-text')[0].style.display = 'none';
+    document.querySelector('#leiaMais').style.display = 'unset';
+    document.querySelector('.split-lists').style.display = 'unset';
+  }
 
 })();
